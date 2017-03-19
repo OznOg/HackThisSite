67beccc1f99f1c189017383539e29b8a975d03e3
Sébastien Gonzalve <sebastien.gonzalve@aliceadsl.fr>
2017-03-19 16:35:28 +0100

    make keygen generate code for both: site & app17
    Signed-off-by: Sébastien Gonzalve <sebastien.gonzalve@aliceadsl.fr>

--- app/app17/app17keygen.cpp
+++ app/app17/app17keygen.cpp
@@ -1,8 +1,28 @@
 
 #include <iostream>
-#include <cstring>
+#include <sstream>
 #include <iomanip>
 
+template <typename T>
+std::string keygen(const std::string &login)
+{
+    std::stringstream ss;
+    ss << std::uppercase << "HTS";
+
+    unsigned int expected_value = 0;
+    for (size_t idx = 0; idx < login.size(); idx++)
+    {
+        if (idx % 2 == 0)
+            ss << "-";
+        unsigned int X = (login[idx] - expected_value) >> 1;
+        unsigned int Y = ~((T)login[idx] << expected_value);
+
+        expected_value = X & Y;
+
+        ss << std::hex << std::setw(2) << std::setfill('0') << static_cast<int>(expected_value);
+    }
+    return ss.str();
+}
 int main(int argc, char **argv)
 {
     if (argc != 2) {
@@ -13,23 +33,8 @@ int main(int argc, char **argv)
 
     const auto login = argv[1]; // login
 
-    unsigned int expected_value = 0;
-
-    std::cout << std::uppercase << "HTS";
-
-    for (size_t idx = 0; idx < strlen(login); idx++)
-    {
-        if (idx % 2 == 0)
-            std::cout << "-";
-        unsigned int X = (login[idx] - expected_value) >> 1;
-        unsigned int Y = ~((long)login[idx] << expected_value);
-
-        expected_value = X & Y;
-
-        std::cout << std::hex << std::setw(2) << std::setfill('0') << static_cast<int>(expected_value);
-    }
-
-    std::cout << std::endl;
+    std::cout << "Code for app17:          " << keygen<uint32_t>(login) << std::endl;
+    std::cout << "Code for HTS validation: " << keygen<uint64_t>(login) << std::endl;
 
     return 0;
 }
